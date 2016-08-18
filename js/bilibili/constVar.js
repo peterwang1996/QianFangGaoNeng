03ad3af5d7896b494465aedc824d4f8dbab2fded
Peter Wang <peterwang1996@outlook.com>
2016-08-18 16:42:13 +0800

    some visual changes

--- js/bilibili/constVar.js
+++ js/bilibili/constVar.js
@@ -1,6 +1,7 @@
 var isOpen = true;
 var domChartId = 'danmuku-chart';
-var domChartInner = '<div id="' + domChartId + '" style="width: 980px; height: 160px; margin: 0 auto;"></div>';
+var chartHeight = 160;
+var domChartInner = '<div id="' + domChartId + '-container" style="height: ' + chartHeight + 'px; width: 980px; margin: 10px auto; background: #fff; border: 1px solid #e5e9ef; border-radius: 4px;"><div id="' + domChartId + '" style="height: ' + chartHeight + 'px"></div></div>';
 var danmukuPart = 60;
 var timeInAdvance = 5;
 
@@ -11,17 +12,29 @@ var chartOption = {
     grid: {
         top: '36%',
         left: '2%',
-        right: '58%',
-        bottom: '0%',
+        right: '8%',
+        bottom: '4%',
         containLabel: true
     },
     xAxis: {
         type: 'category',
         boundaryGap: false,
+        axisLine: {
+            show: false
+        },
+        axisTick: {
+            show: false
+        },
         data: null
     },
     yAxis: {
-        type: 'value'
+        type: 'value',
+        axisLine: {
+            show: false
+        },
+        axisTick: {
+            show: false
+        }
     },
     series: {
         name: '平均弹幕密度（条/秒）',
@@ -29,8 +42,21 @@ var chartOption = {
         smooth: true,
         symbolSize: 12,
         showSymbol: false,
+        itemStyle: {
+            normal: {
+                color: 'rgb(0, 161, 214)'
+            }
+        },
         areaStyle: {
-            normal: {}
+            normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0,
+                    color: 'rgb(0, 161, 214)'
+                }, {
+                    offset: 1,
+                    color: 'rgb(255, 255, 255)'
+                }])
+            }
         },
         markLine: {
             symbol: 'none',
